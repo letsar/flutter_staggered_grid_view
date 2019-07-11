2676fe3ac548788bf7aded2af0b8dc8ca32f6231
Leon Guan <leon.guan@gmail.com>
2019-07-11 18:20:57 +0800

    Continue trying to fix letsar/flutter_staggered_grid_view#49 on flutter after v1.7

--- lib/src/rendering/sliver_variable_size_box_adaptor.dart
+++ lib/src/rendering/sliver_variable_size_box_adaptor.dart
@@ -377,7 +377,7 @@ abstract class RenderSliverVariableSizeBoxAdaptor extends RenderSliver
   bool hitTestChildren(SliverHitTestResult result, 
       { @required double mainAxisPosition, @required double crossAxisPosition }) {
     for (var child in children) {
-      if (hitTestBoxChild(BoxHitTestResult.wrap(result), child,
+      if (hitTestBoxChild(BoxHitTestResult.wrap(HitTestResult.wrap(result)), child,
           mainAxisPosition: mainAxisPosition,
           crossAxisPosition: crossAxisPosition)) return true;
     }
