d597bf687909e59a367e3b17448d6d647a0fe5fb
Romain Rastel <rromain35@gmail.com>
2022-01-01 11:58:25 +0100

    add documentation for aligned

--- examples/lib/examples/aligned.dart
+++ examples/lib/examples/aligned.dart
@@ -12,71 +12,15 @@ class AlignedPage extends StatelessWidget {
     return AppScaffold(
       title: 'Aligned',
       child: AlignedGridView.count(
-        crossAxisCount: 3,
+        crossAxisCount: 4,
         mainAxisSpacing: 4,
         crossAxisSpacing: 4,
         itemBuilder: (context, index) {
-          return AlignedTile(index: index);
+          return Tile(
+            index: index,
+            extent: (index % 7 + 1) * 30,
+          );
         },
-      ),
-    );
-  }
-}
-
-class AlignedTile extends StatefulWidget {
-  const AlignedTile({
-    Key? key,
-    required this.index,
-    this.animate = false,
-  }) : super(key: key);
-
-  final int index;
-  final bool animate;
-
-  @override
-  State<AlignedTile> createState() => _AlignedTileState();
-}
-
-class _AlignedTileState extends State<AlignedTile>
-    with SingleTickerProviderStateMixin {
-  Color color = Colors.blue;
-  late final controller =
-      AnimationController(vsync: this, duration: Duration(seconds: 1));
-  late final animation = Tween<double>(begin: 0, end: 6).animate(controller);
-
-  @override
-  void initState() {
-    super.initState();
-    if (widget.animate) {
-      controller.repeat(reverse: true);
-    }
-  }
-
-  @override
-  void dispose() {
-    controller.dispose();
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: () {
-        final newColor = color == Colors.blue ? Colors.red : Colors.blue;
-        setState(() {
-          color = newColor;
-        });
-      },
-      child: Container(
-        padding: const EdgeInsets.all(4),
-        color: color,
-        child: AnimatedBuilder(
-          animation: animation,
-          builder: (context, child) {
-            return Text('Lorem ipsum ' *
-                ((widget.index % 7 + 1) * 2 + animation.value.ceil()));
-          },
-        ),
       ),
     );
   }
